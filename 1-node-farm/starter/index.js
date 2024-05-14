8993caac156dfc8d72e15b824ccb35eb34eabd51
Jason Duquain <jduquain@gmail.com>
2024-05-14 16:38:02 -0400

    after lesson 14 HTML Templating Building the Templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,9 @@
 
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+///////////////////////////////////////////////////////////////////////////////////// FILES
 
 // blocking sync way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -11,17 +15,45 @@ const fs = require('fs');
 
 
 // non-blocking async way
-fs.readFile('./txt/stat.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('error!!')
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/stat.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('error!!')
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('your file has been written!!')
-      })
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('your file has been written!!')
+//       })
+//     });
+//   });
+// });
+// console.log('will read file');
+
+
+///////////////////////////////////////////////////////////////////////////////////// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('dis is the overview');
+  } else if (pathName === '/product') {
+    res.end('dis is the product');
+  } else if(pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
     });
-  });
+    res.end('<h1>page cannot be found</h1>');
+  }
 });
-console.log('will read file');
+
+server.listen(8000, () => {
+  console.log('listening to requests on port 8000');
+})
